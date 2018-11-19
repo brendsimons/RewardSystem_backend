30aaf2928ce21197b73d0353d6b0dd4cac928e22
CVanHoof <vanhoof_christophe@outlook.com>
2018-11-19 12:49:40 +0100

    Added tasks

--- users/user.model.js
+++ users/user.model.js
@@ -6,6 +6,8 @@ const schema = new Schema({
     hash: { type: String, required: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
+    score: { type: Number , default: 0 },
+    credits: { type: Number, default: 0 },
     createdDate: { type: Date, default: Date.now }
 });
 
