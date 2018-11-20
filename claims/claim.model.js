b4642c801c8e1799f7de209506769c3ac01498e4
Brend Simons <brendsimons@gmail.com>
2018-11-20 12:56:49 +0100

    Claim statusses added in config.json

--- claims/claim.model.js
+++ claims/claim.model.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
+const config = require('config.json');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
     message: { type: String, required: true },
     userId: { type: String, required: true },
-    taskId: { type: String, required: true},
+    taskId: { type: String, required: true },
     status: {
         type: [{
             type: String,
-            enum: ["pending", "rewarded", "not-rewarded"]
-        }], required: true },
+            enum: config.claimStatusses
+        }], default: config.claimStatusses[0], required: true
+    },
     createdDate: { type: Date, default: Date.now }
 });
 
